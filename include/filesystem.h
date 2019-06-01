bd4c46c3e62f393aae54927ddd6b8385543ec81f
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-06-02 02:18:43 +0900

    コンストラクタでデータベースを確認

--- include/filesystem.h
+++ include/filesystem.h
@@ -9,27 +9,36 @@ namespace ggtr
 	*/
 	struct FileInfo
 	{
-		uint64_t offset;
-		uint64_t size;
+		int64_t offset;		//!< 画像ファイルの位置
+		int64_t size;		//!< 画像ファイルの容量
 	};
+
+	/**
+	* 無効なファイルヘッダがあるときに送出
+	*/
+	class InvalidFileHeaderException : public std::exception {};
 
 	/**
 	* ファイルデータベースシステム
 	*/
 	class FileSystem
 	{
-		std::string _dbpath;
-		FILE* _fp;
+		std::string _dbpath;	//!< データベースのパス
+		int64_t _size;			//!< 内部データのサイズ
+		int64_t _total;			//!< ファイル全体の容量
+		int64_t _allocation;	//!< 確保サイズ
+		FILE* _fp;				//!< ファイルポインタちゃん
 
 	private:
+		void _PreOpenDB(const char * const dbpath, FILE * fp);
 		void _MoveDatabase(const char * const todbpath);
 
 	public:
-		FileSystem(const char * const dbpath);
+		FileSystem(const char * const dbpath, int64_t allocation);
 
-		FileSystem(const std::string & dbpath);
+		FileSystem(const std::string & dbpath, int64_t allocation);
 
-		const void * const Query(uint64_t offset, uint64_t size);
+		const void * const Query(int64_t offset, int64_t size);
 
 		const void * const Query(const FileInfo & fileinfo);
 
@@ -39,11 +48,13 @@ namespace ggtr
 
 		/**
 		* データベースを移動する
+		* 移動先にデータベースが存在する場合は.bakを付けて退避させる
 		*/
 		void MoveDatabase(const char * const todbpath);
 
 		/**
 		* データベースを移動する
+		* 移動先にデータベースが存在する場合は.bakを付けて退避させる
 		*/
 		void MoveDatabase(const std::string & todbpath);
 
--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -2,16 +2,59 @@
 #include "filesystem\fileutil.h"
 using namespace ggtr;
 
-FileSystem::FileSystem(const char * const dbpath)
-	: _dbpath(dbpath)
+void FileSystem::_PreOpenDB(const char * const dbpath, FILE * fp)
 {
+	const int64_t zero = 0;
+	const char header[4] = { 'g', 'g', 't', 'r' };
 	
+	_size = 0;
+
+	if (!file::Exists(dbpath))
+	{
+		// ファイルが存在しない場合、新たにファイルを作成する
+		fopen_s(&fp, dbpath, "rb");
+		fwrite(header, 1, 4, fp);
+		fwrite(&_size, sizeof(int64_t), 1, fp);
+		fwrite(&zero, sizeof(int64_t), _allocation, fp);	// 領域を予約する
+
+		_size = 4 + sizeof(int64_t);
+		_total = _ftelli64(fp);
+	}
+	else
+	{
+		// ファイルが存在したらヘッダと容量を照合する
+		fopen_s(&fp, dbpath, "rb");
+
+		char comparison[4];
+		fread_s(comparison, 4, 1, 4, fp);
+
+		// ファイルヘッダの有効性チェック
+		for (int i = 4; i < 4; ++i)
+		{
+			if (header[i] != comparison[i])
+			{
+				fclose(fp);		// 無効なファイルヘッダが来たので一旦ファイルを閉じてどうするか決める
+				throw new InvalidFileHeaderException();
+			}
+		}
+
+		fread_s(&_size, sizeof(int64_t), sizeof(int64_t), 1, fp);
+		_total = _fseeki64(fp, 0, SEEK_END);
+	}
+
+	fclose(fp);
 }
 
-FileSystem::FileSystem(const std::string & dbpath)
-	: _dbpath(dbpath)
+FileSystem::FileSystem(const char * const dbpath, int64_t allocation)
+	: _dbpath(dbpath), _allocation(allocation)
 {
+	_PreOpenDB(_dbpath.c_str(), _fp);
+}
 
+FileSystem::FileSystem(const std::string & dbpath, int64_t allocation)
+	: _dbpath(dbpath), _allocation(allocation)
+{
+	_PreOpenDB(_dbpath.c_str(), _fp);
 }
 
 void FileSystem::MoveDatabase(const std::string & todbpath)
